565ba58ae8081e75a0a7230b39840e114b87f5a4
Rishikesh Vijay <rvijay234@gmail.com>
2026-04-18 14:17:42 +1000

    final commit

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull rvijay2398/simple-python-flask-app:latest
+docker pull rvijay2398/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 rvijay2398/simple-python-flask-app:latest
+docker run -d -p 5000:5000 rvijay2398/simple-python-flask-app
